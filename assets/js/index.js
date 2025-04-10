608ef884928086a1923b35d882d38358135b687a
DevJeipi <joaopedroghilardi@gmail.com.br>
2025-04-10 16:47:59 -0300

    ac&nodeadd

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const API_KEY = '6459226b857ce0283df252b9c1bc9fc63a2cbcd0a3a3e6b697fc08d246c23ccb465f904f';
+const API_URL = 'https://multigamer362.api-us1.com/api/3/contacts';
+
+app.post('/api/enviar', async (req, res) => {
+  console.log("Chegou no backend com:", req.body);
+
+  const { nome, email, telefone } = req.body;
+
+  try {
+    const response = await axios.post(API_URL, {
+      contact: {
+        email,
+        firstName: nome,
+        phone: telefone,
+        lists: [
+          {
+            list: 4, // ID da lista aqui
+            status: 1 // 1=subscribed, 2=unsubscribed 
+          }
+        ]
+      }
+    }, {
+      headers: {
+        'Api-Token': API_KEY,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.error("Erro no try/catch:", err);
+    res.status(500).json({ erro: 'Erro ao enviar para o ActiveCampaign' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
